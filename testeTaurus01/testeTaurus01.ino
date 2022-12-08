aa2d423f6ef521b7387beef9d77d8326d301901a
Gabriel Rosa <gabrielrosarcg@hotmail.com>
2022-12-08 09:38:06 -0300

    Added Moviment Object

--- testeTaurus01/testeTaurus01.ino
+++ testeTaurus01/testeTaurus01.ino
@@ -12,6 +12,49 @@ SharpIR sensor( SharpIR::GP2Y0A21YK0F, A1 );
 SoftwareSerial bluetooth(2, 3);
 int incomingByte;
 
+class Moviment{
+
+  private:
+   int _speed;
+   DCMotorController leftEngine;
+   DCMotorController rightEngine;
+
+  public:
+    void setSpeed(int speed){
+      _speed = speed;
+    }
+
+    int getSpeed(){
+      return _speed;
+    }
+
+    void setEngines(DCMotorController l, DCMotorController r){
+      leftEngine = l;
+      rightEngine = r;
+    }
+
+    void forward(){
+      leftEngine.writo(speed);
+      rightEngine.write(speed);
+    }
+
+    void left(){
+      leftEngine.write(-speed);
+      rightEngine.write(speed);
+    }
+
+    void right(){
+      leftEngine.write(speed);
+      rightEngine.write(-speed);
+    }
+
+    void stop(){
+      leftEngine.brake();
+      rightEngine.brake();
+    }
+}
+
+
 void setup()
 {
   bluetooth.begin(9600);
@@ -20,42 +63,45 @@ void setup()
 void loop()
 {
   int distance = sensor.getDistance();
+  // Direita = true - Esquerda = false
+  bool direction = true, stop = false;
+  Moviment moviment;
+  moviment.setEngines(motor1, motor2);
 
   if (bluetooth.available() > 0) {
     incomingByte = bluetooth.read();
   }
 
-  while (distance>=20) {
+  while(distance>=20) {
     distance = sensor.getDistance();
-    motor1.write(speed);
-    motor2.write(speed);
+    moviment.forward();
   }
 
-  motor1.brake();
-  motor2.brake();
+  if(distance<20){
 
-  while(distance<20){
-    distance = sensor.getDistance();
-    incomingByte = bluetooth.read();
+    switch(incomingByte){
+      case 'B':
+        stop = false;
+        break;
 
-    if (incomingByte == 'B') {
-      motor1.write(-speed);
-      motor2.write(-speed);
-   }
+      case 'L':
+        direction = false;
+        break;
 
-    if (incomingByte == 'L' ) {
-      motor1.write(-speed);
-      motor2.write(speed);
+      case 'R':
+        direction = true;
+        break;
+
+      case 'S':
+        stop = true
+        break;
     }
 
-    if (incomingByte == 'R') {
-      motor1.write(speed);
-      motor2.write(-speed);
-    }
+    if(!stop){
+      if(direction) moviment.right();
 
-    if (incomingByte == 'S' ) {
-      motor1.brake();
-      motor2.brake();
+      else moviment.left();
     }
+    else moviment.stop();
   }
 }
