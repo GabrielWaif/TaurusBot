c5280ae70effd6d40298320987a209748b963279
ArthurHoengen <107923277+ArthurHoengen@users.noreply.github.com>
2022-12-09 15:03:06 -0300

    Update Moviment.h

--- testeTaurus01/Moviment.h
+++ testeTaurus01/Moviment.h
@@ -5,13 +5,15 @@
 class Moviment
 {
 public:
-        Moviment (DCMotorController motor1, DCMotorController motor2);  
+        Moviment (DCMotorController _motor1, DCMotorController _motor2);  
         void forward();
         void left();
         void right();
         void _stop();
         
 private:
+        DCMotorController motor1;
+        DCMotorController motor2;
         int _speed = 255;
         bool direction = true, stop = true;
 };
